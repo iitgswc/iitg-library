9525ea0badd66756aa30885e2b5fd4c50492d172
Kavish Dahekar <kavishdahekar@gmail.com>
2017-03-29 02:57:07 +0530

    Added google sheets functionality

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -20,6 +20,11 @@ class Admin extends CI_Controller {
 	 */
 
 	public $title = "Lakshminath Bezbaroa Central Library";
+	private $google_api_credentials;
+	private $access_key_file;
+	private $accessToken;
+	private $pdf_location = "assets/sheets/";
+	private $sheet_remote_details = "assets/sheets/sheet_details.json";
 
 	public function __construct(){
 		parent::__construct();
@@ -28,8 +33,9 @@ class Admin extends CI_Controller {
 		// $this->load->helper(array('url','language'));
 
 		$this->form_validation->set_error_delimiters($this->config->item('error_start_delimiter', 'ion_auth'), $this->config->item('error_end_delimiter', 'ion_auth'));
-
 		$this->lang->load('auth');
+		$this->google_api_credentials = FCPATH.'.api-keys/client_secret_767286571202-ogsenpbt99tojggribdvqf0g7afh0omn.apps.googleusercontent.com.json';
+		$this->access_key_file = FCPATH.'.api-keys/google_drive_api_access_key.json';
 	}
 
 	public function index(){
@@ -369,8 +375,8 @@ class Admin extends CI_Controller {
 
 	// edit a user
 	public function edit_user($id){
-	if (!$this->ion_auth->logged_in()){
-			redirect('admin/login' , 'refresh');
+		if (!$this->ion_auth->logged_in()){
+				redirect('admin/login' , 'refresh');
 		}
 
 		$this->data['page_title'] = $this->lang->line('edit_user_heading');
@@ -550,5 +556,195 @@ class Admin extends CI_Controller {
 		$view_html = $this->load->view($view, $this->viewdata, $returnhtml);
 
 		if ($returnhtml) return $view_html;//This will return html on 3rd argument being true
+	}
+
+
+	// ********************************************************************************
+	// google sheets api
+	// ********************************************************************************
+
+	// edit a user
+	public function updateExpenditureSheet($status = NULL){
+		if (!$this->ion_auth->logged_in()){
+				redirect('admin/login' , 'refresh');
+		}
+
+		$this->data['show_success'] = false;
+		if($status == "updated"){
+			$this->data['show_success'] = true;
+		}
+
+		if($this->input->post()){
+			$sheet_details['sheet_remote_filename'] = $this->input->post('sheet_remote_filename');
+			$sheet_details['update_cell_location']  = $this->input->post('update_cell_location');
+			file_put_contents($this->sheet_remote_details,json_encode($sheet_details));
+
+			redirect('admin/fetch_update_sheet');
+		}
+
+
+		$this->data['page_title'] = "Update Expenditure Sheet";
+		$sheet_details = json_decode(file_get_contents($this->sheet_remote_details), true);
+		$this->data['sheet_remote_filename'] = $sheet_details['sheet_remote_filename'];
+		$this->data['update_cell_location']  = $sheet_details['update_cell_location'];
+
+		// $this->_render_page('auth/edit_user', $this->data);
+		$this->load->view('admin/admin_header', $this->data);
+		$this->load->view('admin/update_expenditure_sheet', $this->data);
+		$this->load->view('admin/admin_footer', $this->data);
+	}
+
+	// login using OAUTH 2.0 and returns client object
+	public function getClient(){
+
+		if (!$this->ion_auth->logged_in()){
+				redirect('admin/login' , 'refresh');
+		}
+
+		$client = new Google_Client();
+		$client->setAuthConfig($this->google_api_credentials);
+		$client->addScope(Google_Service_Drive::DRIVE);
+		$client->setAccessType('offline');
+		// $client->addScope("https://www.googleapis.com/auth/spreadsheets");
+		// $client->addScope("https://spreadsheets.google.com/feeds");
+		$redirect_uri = site_url('gsheet/getClient');
+		$client->setRedirectUri($redirect_uri);
+
+		if (isset($_GET['code'])) {
+			$accessToken = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+			if(!file_exists(dirname($this->access_key_file))) {
+				mkdir(dirname($this->access_key_file), 0700, true);
+			}
+			file_put_contents($this->access_key_file, json_encode($accessToken));
+			redirect(base_url('gsheet/fetch_update_sheet'));
+		}
+
+		// check if credentials present
+		if (file_exists($this->access_key_file)) {
+			$accessToken = json_decode(file_get_contents($this->access_key_file), true);
+		}else{
+			$authUrl = $client->createAuthUrl();
+			redirect($authUrl);
+		}
+
+		try{
+			$client->setAccessToken($accessToken);
+		} catch(Exception $e){
+			// delete token file and start over
+			echo("delete token file and start over");
+			unlink($this->access_key_file);
+			redirect(base_url('gsheet/fetch_update_sheet'));
+		}
+
+		// Refresh the token if it's expired.
+		if ($client->isAccessTokenExpired()) {
+			$client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+			file_put_contents($this->access_key_file, json_encode($client->getAccessToken()));
+		}
+
+		return $client;
+	}
+
+
+	// fetches google sheet, breaks it into pages and returns first page.
+	public function fetch_update_sheet(){
+
+		if (!$this->ion_auth->logged_in()){
+				redirect('admin/login' , 'refresh');
+		}
+
+		// read google sheet name and cell location from file
+		$sheet_details = json_decode(file_get_contents($this->sheet_remote_details), true);
+		$sheet_remote_filename = $sheet_details['sheet_remote_filename'];
+		$update_cell_location  = $sheet_details['update_cell_location'];
+
+		$client = $this->getClient();
+		$service = new Google_Service_Drive($client);
+
+		$optParams = array(
+			// 'q' => 'name=\'TESTING - Library Book Budget for FY 2016-17.xlsx\'',
+			'q' => 'name=\''.$sheet_remote_filename.'\'',
+			'pageSize' => 10,
+			'fields' => 'nextPageToken, files(id, name)'
+		);
+
+		$results = $service->files->listFiles($optParams);
+
+		if (count($results->getFiles()) == 0) {
+			show_error("Google sheet ".$sheet_remote_filename." not found!");
+		} else {
+			// print "Files:\n";
+			// foreach ($results->getFiles() as $file) {
+			// 	printf("%s (%s)\n", $file->getName(), $file->getId());
+			// }
+		}
+
+		$fileId = $results->getFiles()[0]->getId();
+		$spreadsheetId = $fileId;
+
+		// add last updated
+		$service_sheets = new Google_Service_Sheets($client);
+		$range = $update_cell_location;
+		$values = array(
+			array(
+					'=concat(concat("Last Updated : ",text(now(),"dd/mm/yyyy HH:m:ss"))," (24 hour format)")'
+			)
+		);
+		$body = new Google_Service_Sheets_ValueRange(array('values' => $values));
+		$params = array('valueInputOption' => 'USER_ENTERED'); //https://developers.google.com/sheets/api/reference/rest/v4/ValueInputOption
+		$result = $service_sheets->spreadsheets_values->update($spreadsheetId, $range, $body, $params);
+
+
+		// download pdf
+		$response = $service->files->export($fileId, 'application/pdf', array('alt' => 'media'));
+		$content = $response->getBody()->getContents();
+		$fp = fopen($this->pdf_location.'sheet.pdf', 'w');
+		fwrite($fp, $content);
+		fclose($fp);
+
+		$this->split_pdf('sheet.pdf',$this->pdf_location,1);
+		unlink($this->pdf_location.'sheet.pdf');
+		rename($this->pdf_location.'sheet_1.pdf', $this->pdf_location.'bvstatus.pdf');
+
+		// clean the cell that was changed
+		$values = array(
+			array('')
+		);
+		$body = new Google_Service_Sheets_ValueRange(array('values' => $values));
+		$params = array('valueInputOption' => 'USER_ENTERED'); //https://developers.google.com/sheets/api/reference/rest/v4/ValueInputOption
+		$result = $service_sheets->spreadsheets_values->update($spreadsheetId, $range, $body, $params);
+
+		redirect('admin/updateExpenditureSheet/updated');
+	}
+
+
+	public function split_pdf($filename, $end_directory = false, $pages = 1){
+
+		if (!$this->ion_auth->logged_in()){
+				redirect('admin/login' , 'refresh');
+		}
+
+		$end_directory = $end_directory ? $end_directory : './';
+		
+		$pdf = new FPDI();
+		$pagecount = $pdf->setSourceFile($this->pdf_location.$filename); // How many pages?
+		
+		// Split each page into a new PDF
+		for ($i = 1; $i <= $pagecount; $i++) {
+			$new_pdf = new FPDI();
+			$new_pdf->AddPage();
+			$new_pdf->setSourceFile($this->pdf_location.$filename);
+			$new_pdf->useTemplate($new_pdf->importPage($i));
+			
+			try {
+				$new_filename = $end_directory.str_replace('.pdf', '', $filename).'_'.$i.".pdf";
+				$new_pdf->Output($new_filename, "F");
+				// echo "Page ".$i." split into ".$new_filename."<br />\n";
+			} catch (Exception $e) {
+				echo 'Caught exception: ',  $e->getMessage(), "\n";
+			}
+
+			if($i == $pages) break;
+		}
 	}
 }
--- application/controllers/Gsheet.php
+++ application/controllers/Gsheet.php
@@ -1,6 +1,9 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+use Google\Spreadsheet\DefaultServiceRequest;
+use Google\Spreadsheet\ServiceRequestFactory;
+
 class Gsheet extends CI_Controller {
 
 	/**
@@ -19,15 +22,148 @@ class Gsheet extends CI_Controller {
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
 
+
 	public $title = "Lakshminath Bezbaroa Central Library";
+	private $google_api_credentials;
+	private $access_key_file;
+	private $accessToken;
+	private $pdf_location = "assets/sheets/";
 
 	public function __construct(){
 		parent::__construct();
 
-		$this->form_validation->set_error_delimiters($this->config->item('error_start_delimiter', 'ion_auth'), $this->config->item('error_end_delimiter', 'ion_auth'));
-
-		$this->lang->load('auth');
+		$this->google_api_credentials = FCPATH.'.api-keys/client_secret_767286571202-ogsenpbt99tojggribdvqf0g7afh0omn.apps.googleusercontent.com.json';
+		$this->access_key_file = FCPATH.'.api-keys/google_drive_api_access_key.json';
 	}
 
-	
+	public function index(){
+		
+	}
+
+
+	public function getClient(){
+
+		$client = new Google_Client();
+		$client->setAuthConfig($this->google_api_credentials);
+		$client->addScope(Google_Service_Drive::DRIVE);
+		$client->setAccessType('offline');
+		// $client->addScope("https://www.googleapis.com/auth/spreadsheets");
+		// $client->addScope("https://spreadsheets.google.com/feeds");
+		$redirect_uri = site_url('gsheet/getClient');
+		$client->setRedirectUri($redirect_uri);
+
+		if (isset($_GET['code'])) {
+			$accessToken = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+			if(!file_exists(dirname($this->access_key_file))) {
+				mkdir(dirname($this->access_key_file), 0700, true);
+			}
+			file_put_contents($this->access_key_file, json_encode($accessToken));
+			redirect(base_url('gsheet/fetchSheet'));
+		}
+
+		// check if credentials present
+		if (file_exists($this->access_key_file)) {
+			$accessToken = json_decode(file_get_contents($this->access_key_file), true);
+		}else{
+			$authUrl = $client->createAuthUrl();
+			redirect($authUrl);
+		}
+
+		try{
+			$client->setAccessToken($accessToken);
+		} catch(Exception $e){
+			// delete token file and start over
+			echo("delete token file and start over");
+			unlink($this->access_key_file);
+			redirect(base_url('gsheet/fetchSheet'));
+		}
+
+		// Refresh the token if it's expired.
+		if ($client->isAccessTokenExpired()) {
+			$client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+			file_put_contents($this->access_key_file, json_encode($client->getAccessToken()));
+		}
+
+		return $client;
+	}
+
+
+	public function fetchSheet(){
+		$client = $this->getClient();
+		$service = new Google_Service_Drive($client);
+
+		$optParams = array(
+			'q' => 'name=\'TESTING - Library Book Budget for FY 2016-17.xlsx\'',
+			'pageSize' => 10,
+			'fields' => 'nextPageToken, files(id, name)'
+		);
+
+		$results = $service->files->listFiles($optParams);
+
+		if (count($results->getFiles()) == 0) {
+			print "No files found.\n";
+		} else {
+			print "Files:\n";
+			foreach ($results->getFiles() as $file) {
+				printf("%s (%s)\n", $file->getName(), $file->getId());
+			}
+		}
+
+		$fileId = $results->getFiles()[0]->getId();
+		$spreadsheetId = $fileId;
+
+		// add last updated
+		$service_sheets = new Google_Service_Sheets($client);
+		$range = "A30";
+		// $values = array(
+		// 	'=concat(concat("Last Updated From Script : ",text(now(),"dd/mm/yyyy HH:m:ss"))," (24 hour format)")'
+		// );
+		$values = array(
+			array(
+					'=concat(concat("Last Updated : ",text(now(),"dd/mm/yyyy HH:m:ss"))," (24 hour format)")'
+			)
+		);
+		$body = new Google_Service_Sheets_ValueRange(array('values' => $values));
+		$params = array('valueInputOption' => 'USER_ENTERED'); //https://developers.google.com/sheets/api/reference/rest/v4/ValueInputOption
+		$result = $service_sheets->spreadsheets_values->update($spreadsheetId, $range, $body, $params);
+
+
+		// download pdf
+		$response = $service->files->export($fileId, 'application/pdf', array('alt' => 'media'));
+		$content = $response->getBody()->getContents();
+		$fp = fopen($this->pdf_location.'sheet.pdf', 'w');
+		fwrite($fp, $content);
+		fclose($fp);
+
+		$this->split_pdf('sheet.pdf',$this->pdf_location,1);
+		unlink($this->pdf_location.'sheet.pdf');
+		rename($this->pdf_location.'sheet_1.pdf', $this->pdf_location.'bvstatus.pdf');
+	}
+
+
+	public function split_pdf($filename, $end_directory = false, $pages = 1){
+
+		$end_directory = $end_directory ? $end_directory : './';
+		
+		$pdf = new FPDI();
+		$pagecount = $pdf->setSourceFile($this->pdf_location.$filename); // How many pages?
+		
+		// Split each page into a new PDF
+		for ($i = 1; $i <= $pagecount; $i++) {
+			$new_pdf = new FPDI();
+			$new_pdf->AddPage();
+			$new_pdf->setSourceFile($this->pdf_location.$filename);
+			$new_pdf->useTemplate($new_pdf->importPage($i));
+			
+			try {
+				$new_filename = $end_directory.str_replace('.pdf', '', $filename).'_'.$i.".pdf";
+				$new_pdf->Output($new_filename, "F");
+				echo "Page ".$i." split into ".$new_filename."<br />\n";
+			} catch (Exception $e) {
+				echo 'Caught exception: ',  $e->getMessage(), "\n";
+			}
+
+			if($i == $pages) break;
+		}
+	}
 }
